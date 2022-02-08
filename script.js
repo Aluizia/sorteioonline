4096d67a35ba5d52e0d5039cc2721bd60354ebd9
Aluizia <95940476+Aluizia@users.noreply.github.com>
2022-02-08 11:06:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function sortear(){
+  const min = document.getElementById("minimo").value;
+  const max = document.getElementById("maximo").value;
+  const sort= Math.floor((Math.random() * max)+ min);
+  document.getElementById("resultado").innerHTML=sort;
+}
